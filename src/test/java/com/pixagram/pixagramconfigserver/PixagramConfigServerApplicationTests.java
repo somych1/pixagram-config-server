62ef4cd90e71e2712e027e8a02d7abfa6dfbc089
samat <somych1@gmail.com>
2022-10-28 18:01:36 -0500

    config server up and running

---
+++ src/test/java/com/pixagram/pixagramconfigserver/PixagramConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixagram.pixagramconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixagramConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
